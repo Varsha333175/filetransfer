5ec3ebf5cd9602ae95f294a86c8ff1dc6c1c11e8
Rohit333175 <rohitpvs2000.com>
2024-08-10 21:00:17 -0500

    trying to get production right

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
-// const express = require('express');
-// const connectDB = require('./config/db');
-// const cors = require('cors');
-// const fileUpload = require('express-fileupload');
-
-// const app = express();
-// connectDB();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-// app.use(fileUpload());
-
-// // Routes
-// app.use('/api/auth', require('./routes/authRoutes'));
-// app.use('/files', require('./routes/fileRoutes'));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -31,9 +13,17 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 
-// Routes
+// API Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/files', require('./routes/fileRoutes'));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'file-transfer-client', 'build')));
+
+// The "catchall" handler: for any request that doesn't match one of your API routes, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'file-transfer-client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
